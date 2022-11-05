f520f381deefb22f8d07e48d3a3dbe3ffe387cc6
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-11-05 18:24:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const button = document.querySelector('button')
-button.addEventListener('click', dice)
+const button = document.querySelector("button");
+button.addEventListener("click", dice);
 
 function dice() {
-    let randomNumber1 = Math.floor(Math.random() * 6) + 1
-    let randomNumber2 = Math.floor(Math.random() * 6) + 1
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-    let player1Dice = document.querySelector('.img1')
-    let player2Dice = document.querySelector('.img2')
-    let h1 = document.querySelector('h1')
+    let player1Dice = document.querySelector(".img1");
+    let player2Dice = document.querySelector(".img2");
+    let h1 = document.querySelector("h1");
 
-    player1Dice.setAttribute('src', `images/dice${randomNumber1}.png`)
-    player2Dice.setAttribute('src', `images/dice${randomNumber2}.png`)
+    player1Dice.setAttribute("src", `images/dice${randomNumber1}.png`);
+    player2Dice.setAttribute("src", `images/dice${randomNumber2}.png`);
 
     if (randomNumber1 === randomNumber2) {
-        h1.textContent = '🤝 Draw !'
+        h1.textContent = "🤝 Draw !";
     } else if (randomNumber1 > randomNumber2) {
-        h1.textContent = '👏 Player 1 Wins !'
+        h1.textContent = "👏 Player 1 Wins !";
     } else if (randomNumber1 < randomNumber2) {
-        h1.textContent = '👏 Player 2 Wins !'
+        h1.textContent = "👏 Player 2 Wins !";
     }
 }
